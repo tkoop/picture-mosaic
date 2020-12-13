4fe277af5152f713c3dfd6e23689b3e0b55fa360
Tim Koop <github@timkoop.com>
2020-12-13 17:40:03 -0600

    Getting stuff ready

--- mosaic.js
+++ mosaic.js
@@ -1,1 +1,16 @@
 console.log("Picture Mosaic.")
+
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+try {
+    let fileContents = fs.readFileSync('./config.yaml', 'utf8');
+    let data = yaml.safeLoad(fileContents);
+
+    console.log("The config.yaml data is:")
+    console.log(data);
+} catch (e) {
+    console.log("There was a problem reading config.yaml:")
+    console.log(e);
+}
+
